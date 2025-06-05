f697daa2fbe9603be3864899a4de051dc46b0397
Someone <75336818+Piscuss123@users.noreply.github.com>
2025-06-05 14:34:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
+const params = new URLSearchParams({
+  key: 
+});
 
-
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://Piscuss123.github.io'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
- fetch('https://api.steampowered.com/IGameServersService/GetServerList/v1/?key=72452A9717D5F298689C805539FFFFA4')
+ fetch('https://api.steampowered.com/IGameServersService/GetServerList/v1/?${params}')
      .then(response => {
        if (!response.ok) {
          throw new Error(`HTTP error! status: ${response.status}`);
